91d80bc5320b787996e119fe66c7473d5ad2a6d5
Nick <myfjdthink@163.com>
2023-11-09 18:24:17 +0800

    feat: change 500 error cd time

--- ethereumetl/providers/multi_batch_rpc.py
+++ ethereumetl/providers/multi_batch_rpc.py
@@ -63,11 +63,9 @@ class EndpointStatus():
             if self.err_code == 429 and self.fail_pass_time() > 60 * 10:
                 self.status = 'active'
                 return True
-            if self.err_code == 400 and self.fail_pass_time() > 60 * 5:
+            if self.err_code in [400, 500] and self.fail_pass_time() > 60 * 1:
                 self.status = 'active'
                 return True
-            if self.err_code == 500:
-                return False
         return False
 
     def fail(self, err_code: int):
