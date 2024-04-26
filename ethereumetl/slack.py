3a73ac01a4cbe8b629c96452eceee67b94abd468
qiuhongbingo <qiuhongbingo@outlook.com>
2024-04-26 19:58:00 +0800

    Refactor Slack integration in slack.py to comment out the request.post line

--- ethereumetl/slack.py
+++ ethereumetl/slack.py
@@ -4,4 +4,4 @@ import requests
 def send_to_slack(message):
     url = 'https://hooks.slack.com/services/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'
     text = f'【ethereum-etl】{message}'
-    requests.post(url, json={'text': text})
+    # requests.post(url, json={'text': text})
