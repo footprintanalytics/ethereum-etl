1e32c6bd6ab1d7a043952f2f2ea9834301d935ee
trail <trail@footprint.network>
2024-09-10 17:43:38 +0800

    跳过 special ERC-721 token transfer 代币校验

--- ethereumetl/streaming/eth_streamer_adapter.py
+++ ethereumetl/streaming/eth_streamer_adapter.py
@@ -212,6 +212,13 @@ class EthStreamerAdapter:
                 topics = log.topics
                 if topics is None or len(topics) < 1:
                     continue
+                # 跳过 token address = 0x0000420538cd5abfbc7db219b6a1d125f5892ab0 的交易校验
+                """
+                    事实上这个地址是一个 ERC-721 代币地址，ethereum 出现该种代币交易的时候按照以往的处理方式，不会被收录到 token_transfers 中
+                    为了避免校验不通过，这里直接跳过
+                """
+                if log.address.casefold() == '0x0000420538cd5abfbc7db219b6a1d125f5892ab0':
+                    continue
                 if (topics[0]).casefold() == TRANSFER_EVENT_TOPIC:
                     token_transfers_in_logs_count += 1
             if len(token_transfers) != token_transfers_in_logs_count:
